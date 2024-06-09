f1c546f06fea8357847cca02accc9dd78ed1b3da
EspFerevo <olekchekrenov@gmail.com>
2024-06-09 12:09:10 +0300

    Create HW-02

---
+++ src/components/Feedback/feedback.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+const Feedback = ({ good, neutral, bad, total, positivePercent }) => (
+  <>
+    <p>Good: {good}</p>
+    <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+    <p>Total: {total}</p>
+    <p>Positive: {positivePercent}%</p>
+  </>
+);
+
+Feedback.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercent: PropTypes.number.isRequired,
+};
+
+export default Feedback;
